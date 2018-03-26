9407ab16437e511777edbaca39b9b60d6a842b5a
Aaron Na <syn228@nyu.edu>
2018-03-26 22:13:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var recipes = {burger: 'bread'};
 
 function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({object}, key: value)
-
+  return Object.assign({}, object, [key]: value);
 }
