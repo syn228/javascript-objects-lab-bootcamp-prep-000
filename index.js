1c1f1b82e145d66787d63fdf6ff4ba04e4a258eb
Aaron Na <syn228@nyu.edu>
2018-03-26 23:14:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
   return object;
 }
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, object);
+  delete newObj.key
+  return object
+}
